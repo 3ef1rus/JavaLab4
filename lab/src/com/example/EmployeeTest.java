aaff2fa34433e12a057abed3dc5bb66b7b62bf6e
3ef1rus <10kmmrza10k@mail.ru>
2022-09-26 00:43:21 +0300

    sort files

--- lab/src/com/example/EmployeeTest.java
+++ lab/src/com/example/EmployeeTest.java
@@ -2,7 +2,7 @@ package com.example;
 
 import com.example.domain.Employee;
 
-public class EmployeeTest2 {
+public class EmployeeTest{
     public static void main(String[] args) {
         Employee emp = new Employee(0, null, null, 0);
         
