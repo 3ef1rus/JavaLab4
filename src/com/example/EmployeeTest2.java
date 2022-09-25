4383f47d16cfec786d1fecd6ced7506d275e0f5b
3ef1rus <10kmmrza10k@mail.ru>
2022-09-26 00:19:28 +0300

    insert proj

---
+++ src/com/example/EmployeeTest2.java
@@ -0,0 +1,11 @@
+package com.example;
+
+import com.example.domain.Employee;
+
+public class EmployeeTest2 {
+    public static void main(String[] args) {
+        Employee emp = new Employee(0, null, null, 0);
+asdf
+    }
+
+}
---
+++ src/com/example/domain/Employee.java
@@ -0,0 +1,36 @@
+package com.example.domain;
+
+public class Employee {
+    public int empId;
+    public String name;
+    public String ssn;
+    public double salary;
+
+    public Employee(int empId, String name, String ssn, double salary) {
+        this.empId = empId;
+        this.name = name;
+        this.ssn = ssn;
+        this.salary = salary;
+    }
+
+    public int getEmpId() {
+        return empId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSsn() {
+        return ssn;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+}
