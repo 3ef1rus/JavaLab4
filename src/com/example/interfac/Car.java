57fe7790ff6b1e08a5c0b55b0d1b864ab0d8490d
3ef1rus <10kmmrza10k@mail.ru>
2022-09-26 19:20:11 +0300

    fix

--- src/com/example/interfac/Car.java
+++ src/com/example/interfac/Car.java
@@ -37,11 +37,11 @@ class Facade {
     }
 }
 
-public class FacadeTest {//тест
+/*public class FacadeTest {//тест
     public static void main(String[] args) {
         Facade facade = new Facade();
         facade.startCar();
         System.out.println();
         facade.stoptCar();
     }
-}
+}*/
--- src/com/example/interfac/Strategy.java
+++ src/com/example/interfac/Strategy.java
@@ -23,12 +23,12 @@ class Context {
     }
 }
 
-public class StrategyTest {//тест
+/*public class StrategyTest {//тест
     public static void main(String[] args) {
         Context context = new Context(new DownloadWindownsStrategy());
         context.download("file.txt");
         context = new Context(new DownloadLinuxStrategy());
         context.download("file.txt");
     }
-}
+}*/
 
