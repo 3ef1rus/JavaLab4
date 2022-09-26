4f05cae3fa3a7bd5c22af10035818ca2ebd90e39
3ef1rus <10kmmrza10k@mail.ru>
2022-09-26 17:49:24 +0300

    create duo child class and add them in main file

--- src/com/example/MainExample.java
+++ src/com/example/MainExample.java
@@ -2,6 +2,8 @@ package com.example;
 
 import com.example.clas.Figures;
 import com.example.clas.Voluminous;
+import com.example.clas.Flat;
+import com.example.clas.Spherical;
 public class MainExample {
 
     private static void printFigure(Figures emp) {
@@ -9,9 +11,13 @@ public class MainExample {
         System.out.println("Figure number corners: " + emp.getNumCorners());
     }
     public static void main(String[] args) {
-        Figures square = new Figures("triugl",10);
+        Figures square = new Figures("triugl",4);
         Voluminous cube = new Voluminous("cube",8,10);
+        Flat triugle = new Flat("triugle",3,9);
+        Spherical oval = new Spherical("oval", 0)
         printFigure(square);
         printFigure(cube);
+        printFigure(triugle);
+        printFigure(oval);
     }
 }
