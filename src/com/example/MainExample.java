359ae103ca5550b45d674c9e3164419179e3a65c
3ef1rus <10kmmrza10k@mail.ru>
2022-09-26 17:16:30 +0300

    any changes

--- src/com/example/MainExample.java
+++ src/com/example/MainExample.java
@@ -1,11 +1,16 @@
 package com.example;
 
+import com.example.clas.Figures;
+
 public class MainExample {
+
+    private static void printFigure(Figures emp) {
+        System.out.println("Figure name: " + emp.getName());
+        System.out.println("Figure number corners: " + emp.getNumCorners());
+        System.out.println("Figure volume " + emp.getvolume());
+    }
     public static void main(String[] args) {
-        Figures square = new Figures();
-        square.setNumCorners(4);
-        square.setName("square");
-        System.out.println("Figure number corners: " + square.getNumCorners());
-        System.out.println("Figure Name: " + square.getName());
+        Figures square = new Figures("triugl",5,10);
+        printFigure(square);
     }
 }
--- src/com/example/clas/Figures.java
+++ src/com/example/clas/Figures.java
@@ -1,18 +1,22 @@
-package com.example.class;
+package com.example.clas;
 
 public class Figures {
     public int NumCorners;
     public String name;
+    public int volume;
 
-    public Figures() {
+    public Figures(String name, int volume,int NumCorners) {
+        this.NumCorners=NumCorners;
+        this.name=name;
+        this.volume=volume;
     }
 
     public int getNumCorners() {
         return NumCorners;
     }
 
-    public void setNumCorners(int NumCorners) {
-        this.NumCorners = NumCorners;
+    public int getvolume(){
+        return volume;
     }
 
     public String getName() {
