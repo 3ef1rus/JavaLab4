f4f3ed710d52f1e1f7109fed18aed27a32924541
3ef1rus <10kmmrza10k@mail.ru>
2022-09-26 17:34:45 +0300

    create child class voluminous

--- src/com/example/MainExample.java
+++ src/com/example/MainExample.java
@@ -1,16 +1,17 @@
 package com.example;
 
 import com.example.clas.Figures;
-
+import com.example.clas.Voluminous;
 public class MainExample {
 
     private static void printFigure(Figures emp) {
         System.out.println("Figure name: " + emp.getName());
         System.out.println("Figure number corners: " + emp.getNumCorners());
-        System.out.println("Figure volume " + emp.getvolume());
     }
     public static void main(String[] args) {
-        Figures square = new Figures("triugl",5,10);
+        Figures square = new Figures("triugl",10);
+        Voluminous cube = new Voluminous("cube",8,10);
         printFigure(square);
+        printFigure(cube);
     }
 }
