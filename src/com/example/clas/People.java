d90374dda61b141f45d059ff7582d7425a6ccfa5
3ef1rus <10kmmrza10k@mail.ru>
2022-10-04 15:46:15 +0300

    create intarface facade and implement it in child class

--- src/com/example/clas/People.java
+++ src/com/example/clas/People.java
@@ -1,6 +1,6 @@
 package com.example.clas;
 import com.example.interfac.Copyable;
-public class People implements Copyable {
+public class People implements Copyable   {
     public String name;
     public People (String name){
 this.name=name;
