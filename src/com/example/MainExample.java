72cd826f1f4f6f73e8d379a8495ce24eba572f9a
3ef1rus <10kmmrza10k@mail.ru>
2022-10-04 15:46:35 +0300

    f

--- src/com/example/MainExample.java
+++ src/com/example/MainExample.java
@@ -1,5 +1,6 @@
 package com.example;
 
+import com.example.clas.ActivePeople;
 import com.example.clas.People;
 
 public class MainExample {
@@ -8,9 +9,10 @@ public class MainExample {
         People boy = new People("vasia");
         People girl = boy.copy();
         girl.setName("anna");
-
+ActivePeople Woman = new ActivePeople("Tanya");
+Woman.start();
+Woman.stop();
         System.out.println(boy.name);
-        System.out.println(girl.name);
         System.out.println(girl.name);
     }
 }
